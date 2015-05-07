8ffb0a8769b5aba6273677db869d91776094af41
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-07 14:46:03 +0300

    Enhancements for nfs_export. Implemented #2.

--- network/nfs_export.py
+++ network/nfs_export.py
@@ -21,8 +21,8 @@ options:
   path:
     description: The exported directory path
     required: yes
-  host:
-    description: The client's host 
+  hosts:
+    description: The client hosts (addresses or names) 
     required: yes
   readonly: 
     description: |
@@ -71,26 +71,27 @@ options:
 EXAMPLES='''
 
 # Provide a real (resolvable) hostname  
-- nfs_export: path=/var/local/data-1 host=somehost.local squash=none readonly=yes
+- nfs_export: path=/var/local/data-1 hosts=['somehost.local'] squash=none readonly=yes
 
 # Provide an IP address
-- nfs_export: path=/var/local/data-1 host=10.0.2.15 squash=none readonly=no
+- nfs_export: path=/var/local/data-1 hosts=['10.0.2.15', '10.0.2.16'] squash=none readonly=no
 
 # Provide a CIDR IPv4 vnetwork 
-- nfs_export: path=/var/local/data-1 host=10.0.2.15/24 squash=none readonly=no
+- nfs_export: path=/var/local/data-1 hosts=['10.0.2.15/31'] squash=none readonly=no
 
 # Provide an IPV6 or an IPv6 network 
-- nfs_export: path=/var/local/data-1 host=2001:648:2ffc:1225:a800:cff:fe02:d983/126 squash=all readonly=no
+- nfs_export: path=/var/local/data-1 hosts=['2001:648:2ffc:1225:a800:cff:fe02:d983/126'] squash=all readonly=no
 
 # Example with non-inline module arguments
 - nfs_export: 
     path: /var/local/backups 
-    host: tape.local
     sync: yes
     squash: all
     anon_user: backup
     anon_group: backup
-
+    hosts: 
+    - tape1.local
+    - tape2.local
 '''
 
 import os
@@ -181,11 +182,12 @@ def verify_host(host):
     return False
 
 def main():
+    
     module = AnsibleModule(
         argument_spec = dict(
            dest = dict(default='/etc/exports'),
            path = dict(required=True),
-           host = dict(required=True),
+           hosts = dict(required=True),
            readonly = dict(type='bool', default=False),
            secure = dict(type='bool', default=True),
            sync = dict(type='bool', default=True),
@@ -205,19 +207,29 @@ def main():
     if not os.path.isdir(path): 
         raise ValueError('The given path %s is not a directory' % (path))
 
-    host = p.get('host')
-    assert verify_host(host), 'The host %s doesnt seem valid' % (host)
+    # Read and validate hosts
+    
+    hosts = p.get('hosts')
+    if isinstance(hosts, basestring):
+        hosts = hosts.split(',')
+    for host in hosts:
+        assert verify_host(host), 'The host %s doesnt seem valid' % (host)
 
-    # Build options
+    # Build export options
 
     opts = build_export_opts(p)
-
-    # Generate entry suitable for /etc/exports
-
     export_opts = ['%s=%s' %(k, v) if v else k for k, v in opts.items()]
-    line = '%s %s(%s)\n' % (path, host, ','.join(export_opts))
-    
-    # Read target file and replace matching line
+
+    # Generate entries suitable for /etc/exports
+
+    entries = {}
+    for host in hosts:
+        entries[host] = {
+          'matched': False, 
+          'line': '%-28s %s(%s)\n' % (path, host, ','.join(export_opts)),
+        }
+
+    # Read target file and copy/replace/append lines
     
     if os.path.isfile(target_config_file):
         ifp = open(target_config_file, 'r')
@@ -225,38 +237,43 @@ def main():
         ifp = io.StringIO()
     changed = False
     with ifp, NamedTemporaryFile(delete=False) as ofp:
-        matched = False
+        # Copy or replace existing entries 
         for line1 in ifp:
             if line1.startswith('#') or re.match('$', line1):
-                ofp.write(line1) # copy verbatim
+                ofp.write(line1) # copy
                 continue
             path1, rest1 = line1.strip().split()
             if not path == path1.rstrip('/'):
-                ofp.write(line1) # copy verbatim
+                ofp.write(line1) # copy
                 continue
             m1 = re.match('([:\w][^()\s]+)\(.*\)', rest1)
             assert m1, 'Cannot parse host specifier for %s: %s' % (path1, rest1)
-            if host == m1.group(1):
-                ofp.write(line) # replace match
-                matched = True
-                changed = (line1 != line)
+            host1 = m1.group(1)
+            entry1 = entries.get(host1)
+            if entry1:
+                ofp.write(entry1['line']) # replace
+                entry1['matched'] = True
+                changed = (line1 != entry1['line'])
             else:
-                ofp.write(line1) # copy verbatim
-        if not matched:
-            ofp.write(line) # append line
-            changed = True
+                ofp.write(line1) # copy
+        # Append non-matched entries
+        for host in hosts:
+            if not entries[host]['matched']:
+                ofp.write(entries[host]['line']) # append
+                changed = True
     
-    # Update target 
+    # Update target file 
     
     if not dry_run:
+        os.rename(target_config_file, '/tmp/exports.bak')
         os.rename(ofp.name, target_config_file)
 
     module.exit_json(
         changed = changed, 
         path = path,
-        host = host,
+        hosts = hosts,
         target = target_config_file if not dry_run else ofp.name,
-        params = p)
+        opts = opts)
 
 # Go!
 main()
